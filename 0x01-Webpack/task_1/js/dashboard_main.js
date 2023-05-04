b500c22ca9ad504b34b6bcabb4d4ed6a3726540d
Fedy chihaoui <84166701+fedy69@users.noreply.github.com>
2023-05-04 22:01:16 +0100

    Webpack

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,21 @@
+'use strict';
+import $ from 'jquery';
+import _ from 'lodash';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+const updateCounter = (() => {
+  let times = 0;
+  return () => {
+    times++;
+    $('#count').html(`${times} clicks on the button`);
+  };
+})();
+
+$('button').on('click', _.debounce(updateCounter, 500));
+
+updateCounter();
